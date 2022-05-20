420543b6730290b7abab321dd51c5696f8d72319
Jude Davis <judedavis92@gmail.com>
2022-05-20 17:01:44 +0100

    Algorithm and graph structure

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-
+from search import *
 
 
 def main():
-    pass
+    graph = Graph()
+
+    a = Node('A')
+    b = Node('B')
+    c = Node('C')
+
+    graph.add(Edge(0, (None, a)))
+    graph.add(Edge(.2, (a, b)))
+    graph.add(Edge(.4, (b, c)))
+
+    graph.compile()
+
+    graph.search(a, c.name)
+    print(graph.path)
 
 
 if __name__ == '__main__':
--- search.py
+++ search.py
@@ -1,22 +1,120 @@
-from typing import Set
 from collections import deque
+from typing import Set, List, Iterable, Dict
 
 from dataset import Row
 
 
-class RouteSearcher():
-
-    def __init__(self):
-        self.frontier = deque([])
-        self.visited: Set[Node] = set()
-    
-    def search(start, target):
-        raise NotImplementedError
-
 
 class Node:
+
+    def __init__(self, name, neighbours={}):
+        self.name: str = name
+        self.edges: List[Edge] = []
+
+    def __repr__(self):
+        return f'\nNode(\n\tname: {self.name},\n\tedges: {self.edges}\n)\n'
+
+class Edge:
+
+    def __init__(self, cost: float, from_to: tuple):
+        self.cost: float = cost
+        self.from_to: tuple = from_to
     
-    def __init__(self, name, cost, next):
-        self.name = name
-        self.cost = cost
-        self.next = next
+    def __repr__(self):
+        if self.from_to[0]:
+            return f'Edge(from: {self.from_to[0].name}, to: {self.from_to[1].name})'
+        return f'Edge({self.from_to[1].name})'
+
+
+class Graph:
+
+    def __init__(self):
+        self.edges = set()
+        self.verticies = set()
+        self.verticies_by_name = set()
+
+        self.graph: dict = {}
+        self.head: Node = None
+
+        # For the search process
+        self.path = []
+        self.visited = set()
+        self.frontier = deque([])
+
+        self._compile_frontier = deque([])
+    
+    def add(self, edge: Edge):
+        if self.head == None:
+            self.head = edge.from_to[1]
+            self.frontier = deque([self.head])
+
+            self.verticies.add(edge.from_to[1])
+            self.verticies_by_name.add(edge.from_to[1].name)
+
+            node: Node = edge.from_to[1]
+            node.edges.append(edge)
+
+            self.path.append(self.head.name)
+            self.frontier.append(self.head)
+            self._compile_frontier.append(self.head)
+            
+            return
+        
+        node: Node = edge.from_to[0]
+        node.edges.append(edge)
+
+        self.edges.add(edge)
+        self.verticies.add(edge.from_to[0])
+        self.verticies.add(edge.from_to[1])
+
+        self.verticies_by_name.add(edge.from_to[0].name)
+        self.verticies_by_name.add(edge.from_to[1].name)
+    
+    def search(self, start: Node, target: str):
+        cur_node = start  # Just because of semantics
+        
+        if len(self.frontier) == 0:
+            return
+        
+        node = self.frontier.pop()
+        self.path.append(node.name)
+
+        if node.name == target:
+            return self.path
+        
+        self.visited.add(node)
+
+        for e in node.edges:
+            n = e.from_to[1]
+
+            if n not in self.frontier and n not in self.visited:
+                self.frontier.append(n)
+
+        self.search(node, target)
+    
+    # Depth-first compilation of all nodes
+    def compile(self):
+        node = self._compile_frontier.pop()
+        self.visited.add(node)
+
+        # Update graph
+        self.graph.update({node.name: []})
+
+        for edge in node.edges:
+            n = edge.from_to[1]
+            self.graph[node.name].append(n.name)
+        
+        # Update frontier
+        for e in node.edges:
+            n = e.from_to[1]
+
+            if n not in self._compile_frontier and n not in self.visited:
+                self.frontier.append(n)
+
+        if len(self._compile_frontier) == 0:
+            return
+        
+        self.compile()
+
+
+
